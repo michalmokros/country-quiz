8363d789ad29348bf340cfa9a6144574b5af9288
Martin Kacenga <456274@mail.muni.cz>
2021-11-30 16:14:51 +0100

    added login page, translations and hooks. Also added firebase config

--- src/localization/sk.ts
+++ src/localization/sk.ts
@@ -1,3 +1,10 @@
 export default {
-	country_quiz: 'Štátny Kvíz'
+	country_quiz: 'Štátny Kvíz',
+	login: 'Prihlásenie',
+	required: 'Povinné',
+	unknown_error: 'Neznáma chyba',
+	email: 'E-mail',
+	password: 'Heslo',
+	sign_up: 'Zaregistrovať sa',
+	sign_in: 'Prihlásiť sa'
 };
