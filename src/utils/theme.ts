70e8a6a3e3a9976176e1ba8a6633ff2b39c81c9d
Stanislav Petrek <stanislav.petrek@tietoevry.com>
2021-12-07 19:37:24 +0100

    added teams for typoghprahy variants

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -18,4 +18,34 @@ const theme = createTheme({
 	}
 });
 
+theme.typography.h1 = {
+	'fontSize': '2.5rem',
+	'@media (min-width:600px)': {
+		fontSize: '3.0rem'
+	},
+	[theme.breakpoints.up('md')]: {
+		fontSize: '3.5rem'
+	}
+};
+
+theme.typography.h2 = {
+	'fontSize': '2.0rem',
+	'@media (min-width:600px)': {
+		fontSize: '2.5rem'
+	},
+	[theme.breakpoints.up('md')]: {
+		fontSize: '3.0rem'
+	}
+};
+
+theme.typography.h3 = {
+	'fontSize': '1.0rem',
+	'@media (min-width:600px)': {
+		fontSize: '1.5rem'
+	},
+	[theme.breakpoints.up('md')]: {
+		fontSize: '2.5rem'
+	}
+};
+
 export default theme;
